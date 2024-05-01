eeca5b11b242da7713932eadb2296fb8afa22755
LuisGomes2688 <luisfelipegm268@gmail.com>
2024-05-01 14:32:33 -0300

    Função adicionada nos botões

---
+++ script.js
@@ -0,0 +1,15 @@
+const button1 = document.querySelector("#button-1")
+const button2 = document.querySelector("#button-2")
+const button3 = document.querySelector("#button-3")
+
+button1.addEventListener('click', () => {
+    window.open('https://www.google.com', '_blank');
+})
+
+button2.addEventListener('click', () => {
+    window.open('https://www.google.com', '_blank');
+})
+
+button3.addEventListener('click', () => {
+    window.open('https://www.google.com', '_blank');
+})
